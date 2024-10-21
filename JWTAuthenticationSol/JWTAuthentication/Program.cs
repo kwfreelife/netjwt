d3e9acd7ff54ef14f633bad18adf85a4dae1c938
kwfreelife <kwfreelife@gmail.com>
2024-10-21 18:47:27 +0700

    Add token and api employee.

--- JWTAuthenticationSol/JWTAuthentication/Program.cs
+++ JWTAuthenticationSol/JWTAuthentication/Program.cs
@@ -1,4 +1,12 @@
 
+using JWTAuthentication.Interfaces;
+using JWTAuthentication.Models;
+using JWTAuthentication.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace JWTAuthentication
 {
     public class Program
@@ -9,10 +17,35 @@ namespace JWTAuthentication
 
             // Add services to the container.
 
+            //Donot forgot to add ConnectionStrings as "dbConnection" to the appsetting.json file
+            builder.Services.AddDbContext<DatabaseContext>
+                (
+                    //options => options.UseSqlServer(builder.Configuration["ConnectionString:dbConnection"])
+                    options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection"))
+                );
+
+            builder.Services.AddTransient<IEmployees, EmployeeRepository>();
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            /// Add JWT Token for Authentication
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
 
             var app = builder.Build();
 
@@ -22,6 +55,8 @@ namespace JWTAuthentication
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
